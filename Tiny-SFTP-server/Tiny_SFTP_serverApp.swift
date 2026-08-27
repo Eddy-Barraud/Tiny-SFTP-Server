ec534ef0fde670199b4e23b1f23525854c406bf7
Eddy Barraud <19889337+Eddy-Barraud@users.noreply.github.com>
2026-08-27 20:50:34 +0200

    Initial Commit

---
+++ Tiny-SFTP-server/Tiny_SFTP_serverApp.swift
@@ -0,0 +1,32 @@
+//
+//  Tiny_SFTP_serverApp.swift
+//  Tiny-SFTP-server
+//
+//  Created by Eddy Barraud on 27/08/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Tiny_SFTP_serverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
